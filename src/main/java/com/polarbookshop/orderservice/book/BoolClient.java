92e68446257854e6b703a64701073e3c9b01af7a
stk346 <stk346@gmail.com>
2024-09-10 00:36:22 +0900

    feat: ch8.4.1 타임아웃

--- src/main/java/com/polarbookshop/orderservice/book/BoolClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BoolClient.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Component
 public class BoolClient {
     private static final String BOOKS_ROOT_API = "/books/";
@@ -18,6 +20,7 @@ public class BoolClient {
                 .get()// GET 메서드로 요청
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()// 요청을 보내고 응답 받는다.
-                .bodyToMono(Book.class); // 받은 객체를 Mono<book>으로 반환
+                .bodyToMono(Book.class) // 받은 객체를 Mono<book>으로 반환
+                .timeout(Duration.ofSeconds(3), Mono.empty()); // GET 요청에 대해 3초의 타임아웃을 설정, 빈 결과 반환
     }
 }
