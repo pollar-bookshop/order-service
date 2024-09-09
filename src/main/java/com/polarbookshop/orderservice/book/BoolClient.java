14f804a93651dcbc9b721a8b5ff31837c8effea9
stk346 <stk346@gmail.com>
2024-09-10 00:44:01 +0900

    feat: ch8.4.2 재시도

--- src/main/java/com/polarbookshop/orderservice/book/BoolClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BoolClient.java
@@ -3,6 +3,7 @@ package com.polarbookshop.orderservice.book;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -21,6 +22,7 @@ public class BoolClient {
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()// 요청을 보내고 응답 받는다.
                 .bodyToMono(Book.class) // 받은 객체를 Mono<book>으로 반환
-                .timeout(Duration.ofSeconds(3), Mono.empty()); // GET 요청에 대해 3초의 타임아웃을 설정, 빈 결과 반환
+                .timeout(Duration.ofSeconds(3), Mono.empty()) // GET 요청에 대해 3초의 타임아웃을 설정, 빈 결과 반환
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100))); // 100밀리초가 백오프로 총 3회까지 시도한다.
     }
 }
